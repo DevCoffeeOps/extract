d9c28b18af4857874badf0238e7c756dc571f65a
c <wip>
2024-08-07 20:21:10 -0400

    update

--- last-three-builds/02-20240808-ff8107c2c82f0e222343749480b0634e18cbb046/dist/places/getPlaces.js
+++
@@ -1,7 +0,0 @@
-import { googleMapsSearchText } from './googleMapsSearchText.js';
-import { getCoordinates } from '../coordinates/getCoordinates.js';
-export async function getPlaces(keyword, cityState, radius = 5000) {
-    const coordinates = await getCoordinates(cityState);
-    const results = await googleMapsSearchText(keyword, coordinates.lat, coordinates.lng, radius);
-    return results;
-}
