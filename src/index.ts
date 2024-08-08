6dfb8969ac9fd74ea0034bafce44de70dcd199d3
c <wip>
2024-08-07 23:58:02 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
-import { promises as fs } from 'fs';
 import { getPlaces } from './places/getPlaces.js';
-import { writeToOutput } from './utils/index.js';
-
-const EXTRACT__KEYWORD = process.env.EXTRACT__KEYWORD;
-const EXTRACT__CITY_STATE = process.env.EXTRACT__CITY_STATE;
-const EXTRACT__GOOGLE_MAPS_API_KEY = process.env.EXTRACT__GOOGLE_MAPS_API_KEY;
-const EXTRACT__OUTPUT_DIR = process.env.EXTRACT__OUTPUT_DIR;
-
-if (!EXTRACT__KEYWORD || !EXTRACT__CITY_STATE || !EXTRACT__GOOGLE_MAPS_API_KEY || !EXTRACT__OUTPUT_DIR) {
-    throw `ERR: missing required env variable. received ${EXTRACT__KEYWORD}, ${EXTRACT__CITY_STATE}, ${EXTRACT__GOOGLE_MAPS_API_KEY}, ${EXTRACT__OUTPUT_DIR}`
-}
+import { writeToStorage } from './utils/index.js';
 
 async function main() {
-    await fs.mkdir(EXTRACT__OUTPUT_DIR!, { recursive: true });
-    console.log(`extractPlaces("${EXTRACT__KEYWORD}", "${EXTRACT__CITY_STATE}")\n`)
-    const runningShoeStores = await getPlaces(EXTRACT__KEYWORD!, EXTRACT__CITY_STATE!);
-    await writeToOutput(JSON.stringify(runningShoeStores, null, 2));
+    console.log(`extractPlaces("${process.env.EXTRACT__KEYWORD}", "${process.env.EXTRACT__CITY_STATE}")\n`)
+    const places = await getPlaces(process.env.EXTRACT__KEYWORD!, process.env.EXTRACT__CITY_STATE!);
+    await writeToStorage(JSON.stringify(places, null, 2));
 }
 
 main().then().catch(err => console.log(err))
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,31 @@
 import fs from 'fs/promises';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-export async function writeToOutput(str: string) {
-    const filepath = path.join(process.env.EXTRACT__OUTPUT_DIR!, 'data.json')
-    await fs.writeFile(filepath, str);
-    console.log('written to.. file://' + filepath);
+export async function writeToStorage(str: string) {
+    if (process.env.EXTRACT__STORAGE_TYPE === 'file') {
+        await fs.writeFile(process.env.EXTRACT__FILE_PATH!, str);
+        console.log('written to.. file://' + process.env.EXTRACT__FILE_PATH);
+    }
+    else if (process.env.EXTRACT__STORAGE_TYPE === 'mongo') {
+        const client = new MongoClient(process.env.EXTRACT__MONGO_URI!, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        try {
+            const places = JSON.parse(str).places;
+            console.log(JSON.stringify(places, null, 2))
+            await client.connect();
+            
+            const database = client.db(process.env.EXTRACT__DB_NAME);
+            const collection = database.collection(process.env.EXTRACT__COLLECTION_NAME!);
+
+            const insertManyResult = await collection.insertMany(places);
+            console.log(`Successfully wrote to MongoDB! ${insertManyResult}`);
+        } finally {
+            await client.close();
+        }
+    }
 }
